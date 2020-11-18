e78213a0687d4f11f87ed5318059c6a1ebaad50c
ctlove0523 <478309639@qq.com>
2020-11-18 20:50:32 +0800

    support retry send

--- src/main/java/io/gtihub/ctlove0523/bus/EventBusSender.java
+++ src/main/java/io/gtihub/ctlove0523/bus/EventBusSender.java
@@ -5,10 +5,11 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 
 import io.github.ctlove0523.commons.serialization.JacksonUtil;
 import io.github.ctlove0523.discovery.api.Instance;
@@ -32,8 +33,19 @@ public class EventBusSender {
 	private final int receiverPort;
 	private final Map<String, NetSocket> receivers = new HashMap<>();
 	private final LocalEventBus localEventBus;
-	private final Map<String, List<BroadcastEvent>> waitAckEvents = new HashMap<>();
+
+	/**
+	 * 等待发送的事件
+	 */
 	private final Map<String, List<BroadcastEvent>> waitSendEvents = new HashMap<>();
+
+	/**
+	 * 等待接收者确认的事件，key为接收者的唯一标识
+	 */
+	private final Map<String, List<BroadcastEvent>> waitAckEvents = new HashMap<>();
+
+	private ScheduledExecutorService workers = Executors.newScheduledThreadPool(3);
+
 
 	public EventBusSender(String serviceDomainName, ServiceResolver serviceResolver, LocalEventBus localEventBus) {
 		this(serviceDomainName, serviceResolver, DEFAULT_PORT, localEventBus);
@@ -45,6 +57,9 @@ public class EventBusSender {
 		this.receiverPort = receiverPort;
 		this.localEventBus = localEventBus;
 		initReceivers();
+		workers.scheduleWithFixedDelay(this::initReceivers, 0L, 10L, TimeUnit.SECONDS);
+		workers.scheduleWithFixedDelay(this::reSend, 0L, 5L, TimeUnit.SECONDS);
+		workers.scheduleWithFixedDelay(this::reBroadcast, 0L, 5L, TimeUnit.SECONDS);
 	}
 
 	public void post(Object event) {
@@ -83,6 +98,7 @@ public class EventBusSender {
 				waitSendEventList.add(broadcastEvent);
 				waitSendEvents.put(s, waitSendEventList);
 
+				// 通过socket广播事件
 				socket.write(JacksonUtil.object2Json(broadcastEvent), new Handler<AsyncResult<Void>>() {
 					@Override
 					public void handle(AsyncResult<Void> event) {
@@ -99,42 +115,23 @@ public class EventBusSender {
 	public void initReceivers() {
 		List<Instance> instances = serviceResolver.resolve(serviceDomainName);
 		instances.stream()
-				.map(new Function<Instance, String>() {
-					@Override
-					public String apply(Instance instance) {
-						return instance.getAddress().getIpv4();
-					}
-				})
-				.filter(new Predicate<String>() {
-					@Override
-					public boolean test(String host) {
-						return !host.equals(IpUtils.getCurrentListenIp());
-					}
-				}).forEach(new Consumer<String>() {
-			@Override
-			public void accept(String host) {
-				NetClient receiver = vertx.createNetClient();
-				receiver.connect(receiverPort, host, new Handler<AsyncResult<NetSocket>>() {
-					@Override
-					public void handle(AsyncResult<NetSocket> event) {
+				.map(instance -> instance.getAddress().getIpv4())
+				.filter(host -> !host.equals(IpUtils.getCurrentListenIp()))
+				.forEach(host -> {
+					NetClient receiver = vertx.createNetClient();
+					receiver.connect(receiverPort, host, event -> {
 						if (event.succeeded()) {
 							log.info("connect to receiver {} success", host);
 							receivers.put(host, event.result());
-							event.result().handler(new Handler<Buffer>() {
-								@Override
-								public void handle(Buffer event) {
-									acknowledgeReceiverAck(event);
-								}
-							});
+							// 处理来自接收者的响应
+							event.result().handler(this::acknowledgeReceiverAck);
 						}
 						else {
 							log.warn("connect to receiver {} failed", host);
 							receivers.remove(host);
 						}
-					}
+					});
 				});
-			}
-		});
 	}
 
 	private void acknowledgeReceiverAck(Buffer data) {
@@ -142,5 +139,30 @@ public class EventBusSender {
 		BroadcastEvent broadcastEvent = JacksonUtil.json2Object(jsonFormatData, BroadcastEvent.class);
 		String receiverHost = broadcastEvent.getReceiverHost();
 		waitAckEvents.get(receiverHost).remove(broadcastEvent);
+	}
+
+	private void reSend() {
+		Map<String, List<BroadcastEvent>> snapshot = new HashMap<>(waitSendEvents);
+		snapshot.forEach((s, broadcastEvents) -> broadcastEvents.forEach(broadcastEvent -> receivers.get(s).write(JacksonUtil.object2Json(broadcastEvent), event -> {
+			if (event.succeeded()) {
+				waitSendEvents.get(s).remove(broadcastEvent);
+			}
+		})));
+	}
+
+	private void reBroadcast() {
+		Map<String, List<BroadcastEvent>> snapshot = new HashMap<>(waitAckEvents);
+
+		snapshot.forEach(new BiConsumer<String, List<BroadcastEvent>>() {
+			@Override
+			public void accept(String s, List<BroadcastEvent> broadcastEvents) {
+				broadcastEvents.forEach(new Consumer<BroadcastEvent>() {
+					@Override
+					public void accept(BroadcastEvent broadcastEvent) {
+						receivers.get(s).write(JacksonUtil.object2Json(broadcastEvent));
+					}
+				});
+			}
+		});
 	}
 }
